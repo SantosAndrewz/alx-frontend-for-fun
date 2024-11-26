29fb836c34fdc4f2588e3d436de4fd8446a655fb
sentosandrewz@gmail.com <sentosandrewz@gmail.com>
2024-11-26 15:12:23 +0000

    Headings

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Repository for additional/optional projects for ALX Front-end specialization
+# My title
+## My title2
+# My title3
+#### My title4
+### My title5
--- markdown2html.py
+++ markdown2html.py
@@ -24,12 +24,24 @@ if __name__ == "__main__":
         print(f"Missing {file_markdown}", file=sys.stderr)
         exit(1)
 
-    # Opens Markdown file then creates an empty file.
+    # Opens Markdown file then creates an HTML file.
     try:
         with open(file_markdown, 'r', encoding='utf-8') as file_md:
-            md_content = file_md.read()
-        with open(file_html, 'w', encoding='utf-8') as file_html_out:
-            file_html_out.write("")
+            with open(file_html, 'w', encoding='utf-8') as file_html_out:
+                for line in file_md:
+                    line = line.strip()
+                    if line.startswith('#'):
+                        heading_level = len(line.split(' ')[0])
+                        if heading_level <= 6:
+                            heading_content = line[heading_level:].strip()
+                            file_html_out.write(
+                                    f"<h{heading_level}>"
+                                    f"{heading_content}</h{heading_level}>\n"
+                            )
+                        else:
+                            file_html_out.write(line + "\n")
+                    else:
+                        file_html_out.write(line + "\n")
     except Exception as e:
         print(f"Error: {e}", file=sys.stderr)
         exit(1)
