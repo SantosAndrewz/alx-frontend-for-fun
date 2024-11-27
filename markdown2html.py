631f9a8882e4839912e792a37cee37fd58c951a1
sentosandrewz@gmail.com <sentosandrewz@gmail.com>
2024-11-27 21:17:00 +0000

    Refactored parsing  bold syntax

--- markdown2html.py
+++ markdown2html.py
@@ -54,8 +54,10 @@ if __name__ == "__main__":
                             file_html_out.write('<ul>\n')
                             is_in_ul = True
                         list_item = line[2:].strip()
-                        list_item = list_item.replace('**', '<b>')
-                        list_item = list_item.replace("__", "<em>")
+                        list_item = list_item.replace('**', '<b>', 1)
+                        list_item = list_item.replace("**", "</b>", 1)
+                        list_item = list_item.replace("__", "<em>", 1)
+                        list_item = list_item.replace("__", "</em>", 1)
                         file_html_out.write(f" <li>{list_item}</li>\n")
                         continue
 
@@ -68,8 +70,10 @@ if __name__ == "__main__":
                             file_html_out.write('<ol>\n')
                             is_in_ol = True
                         list_item = line[2:].strip()
-                        list_item = list_item.replace('**', '<b>')
-                        list_item = list_item.replace("__", "<em>")
+                        list_item = list_item.replace('**', '<b>', 1)
+                        list_item = list_item.replace('**', '</b>', 1)
+                        list_item = list_item.replace("__", "<em>", 1)
+                        list_item = list_item.replace("__", "</em>", 1)
                         file_html_out.write(f" <li>{list_item}</li>\n")
                         continue
 
@@ -80,8 +84,10 @@ if __name__ == "__main__":
                         if is_in_ol:
                             file_html_out.write("</ol>\n")
                             is_in_ol = False
-                        line = line.replace('**', '<b>')
-                        line = line.replace("__", "<em>")
+                        line = line.replace('**', '<b>', 1)
+                        line = line.replace('**', '</b>', 1)
+                        line = line.replace("__", "<em>", 1)
+                        line = line.replace('__', '</em>', 1)
                         para_text.append(line)
 
                     else:
